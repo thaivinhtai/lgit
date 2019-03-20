2ba83cfe0131747b1a0054e9617b8bbee90bd87c
Tai <thaivinhtai@gmail.com>
2019-03-20 14:46:06 +0700

    update

---
+++ lgit-core/init.py
@@ -0,0 +1,18 @@
+"""This module simulates a very small concept of "git init" command, which will
+create a "lgit repository" via make a specified directory.
+
+Functions in this module:
+    -   .....
+    -   .....
+"""
+
+from tools import *
+
+
+def init(path=''):
+    """
+    init(path)  -> adfsadtwh
+    """
+    mkdir('.lgit')
+
+init()
---
+++ lgit-core/tools/__init__.py
@@ -0,0 +1,5 @@
+"""This module is seen as a constructor of the directory and marks it as a
+Python package. This module also contain some stuff to help the project easier
+to develop."""
+
+from os import *
